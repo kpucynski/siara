6f6f7f5b64ec2fc8eeb1fce9995f2333058cf4cf
Karol Pucynski <kpucynski@gmail.com>
2019-10-14 14:52:19 +0200

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
